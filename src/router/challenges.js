7d1fc10f8f76eaf1ea991e40f9b125b6c234611e
yansong <yansongc@gmail.com>
2023-11-17 11:12:41 +0800

    adjust item names

--- src/router/challenges.js
+++ src/router/challenges.js
@@ -1,12 +1,12 @@
 export default [
     {
-        name: '100 Days CSS',
+        name: '001 100 Days CSS',
         path: '001',
         component: () => import('@/views/100/001.vue'),
         meta: {desc: 'An easy start into the challenge with a custom build number with gradient.'}
     },
     {
-        name: 'Menu Icon',
+        name: '002 Menu Icon',
         path: '002',
         component: () => import('@/views/100/002.vue'),
         meta: {desc: 'Used on almost every website by now, simple but impressively animated it becomes a real eye-catcher.'}
